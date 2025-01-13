e65945c47e626b1b4d4f051ead8144dd5ae9eadb
Marvin Thomaz do Nascimento <marvin.nascimento@invillia.com>
2025-01-13 10:47:11 -0300

    criação dos modelos

---
+++ src/Inventory/Features/Inventory/Data.cs
@@ -0,0 +1,8 @@
+﻿namespace Inventory.Features.Inventory
+{
+    internal sealed class Inventory
+    {
+        public string ProductId { get; set; }
+        public int Stock { get; set; }
+    }
+}
---
+++ src/Payment/Features/Payment/Data.cs
@@ -0,0 +1,9 @@
+﻿namespace Payment.Features.Payment
+{
+    internal sealed class Payment
+    {
+        public int OrderId { get; set; }
+        public string Status { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
