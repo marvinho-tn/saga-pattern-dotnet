fca2cfe943f2d04101262bee68e3def4a8b06f7f
Marvin Thomaz do Nascimento <marvin.nascimento@invillia.com>
2025-01-14 09:41:08 -0300

    Criação de evento de pedido registrado

--- src/Orchestrator/Program.cs
+++ src/Orchestrator/Program.cs
@@ -1,6 +1,9 @@
+using Confluent.Kafka;
 using FastEndpoints;
 using Orchestrator;
+using Orchestrator.Features.Orders.Consumers;
 using Orchestrator.Features.Orders.Domain;
+using Orchestrator.Features.Orders.Handlers;
 using Orchestrator.Http;
 using Refit;
 
@@ -13,6 +16,13 @@ var apisConfig = builder.Configuration.GetRequiredSection("Apis").Get<ApisConfig
 builder.Services.AddCustomRefitClient<OrderService.IService>(apisConfig!.Order.Url);
 builder.Services.AddCustomRefitClient<InventoryService.IService>(apisConfig.Inventory.Url);
 builder.Services.AddCustomRefitClient<PaymentService.IService>(apisConfig.Payment.Url);
+
+builder.Services.AddSingleton(builder.Configuration.GetRequiredSection("Kafka").Get<ProducerConfig>()!);
+builder.Services.AddSingleton(builder.Configuration.GetRequiredSection("Kafka").Get<ConsumerConfig>()!);
+
+builder.Services.AddTransient<IEventHandler<OrderRegisteredEventHandler.Event>, OrderRegisteredEventHandler.Handler>();
+
+builder.Services.AddHostedService<OrderRegisteredConsumer.Consumer>();
 
 var app = builder.Build();
 
