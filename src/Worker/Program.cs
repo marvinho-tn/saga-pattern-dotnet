6c09e439de73e0a7c905067cbd79c31127404343
Marvin Thomaz do Nascimento <marvin.nascimento@invillia.com>
2025-01-14 11:37:45 -0300

    Criação do projeto de worker

---
+++ src/Worker/Program.cs
@@ -0,0 +1,25 @@
+using Confluent.Kafka;
+using Worker;
+using Worker.Consumers;
+using Worker.Domain;
+using Worker.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var apisConfig = builder.Configuration.GetRequiredSection("Apis").Get<ApisConfig>();
+
+builder.Services.AddSingleton(builder.Configuration.GetRequiredSection("Kafka").Get<ConsumerConfig>()!);
+
+builder.Services.AddCustomRefitClient<OrderService.IService>(apisConfig!.Order.Url);
+builder.Services.AddCustomRefitClient<InventoryService.IService>(apisConfig.Inventory.Url);
+builder.Services.AddCustomRefitClient<PaymentService.IService>(apisConfig.Payment.Url);
+
+builder.Services.AddHostedService<OrderRegisteredConsumer.Consumer>();
+builder.Services.AddHostedService<OrderCreatedConsumer.InventoryConsumer>();
+builder.Services.AddHostedService<OrderCreatedConsumer.PaymentConsumer>();
+builder.Services.AddHostedService<ReserveAbortedConsumer.InventoryConsumer>();
+builder.Services.AddHostedService<ReserveAbortedConsumer.PaymentConsumer>();
+
+var app = builder.Build();
+
+app.Run();
